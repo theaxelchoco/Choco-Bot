23c3a1f7b53efd4bf2dc02b7cd1988babe279045
theaxelchoco <axelwalejames@gmail.com>
2023-05-26 00:01:45 -0300

    Finished Trivia Button and Fixed code

--- commands/trivia.js
+++ commands/trivia.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js")
+const {SlashCommandBuilder, ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType} = require("discord.js")
 const {request, fetch} = require("undici")
 
 /*
@@ -24,30 +24,86 @@ module.exports = {
         .setName("trivia")
         .setDescription("Gives a random trivia question. Try to solve!"),
     
-    async execute(interaction) {
-
-        let Query
-        const result = await fetch("https://opentdb.com/api.php?amount=1")
-        Query = await result.json();
-        const Details = Query.results[0]
-
-        const row = new ActionRowBuilder()
-        console.log(Details)
-
-        //Load Options
-        const buttons = [
-            new ButtonBuilder().setCustomId("CorrectAnswer").setLabel(Details.correct_answer).setStyle('Secondary'),
-            ...Details.incorrect_answers.map(answer => new ButtonBuilder().setCustomId(answer).setLabel(answer).setStyle('Secondary'))
-          ];
-
-        // Randomize the position of the buttons using splice
-        for (let i = buttons.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [buttons[i], buttons[j]] = [buttons[j], buttons[i]];
-        }
-
-        row.addComponents(buttons);
+        async execute(interaction) {
+            const client = interaction.user.client;
         
-        await interaction.reply({content: `${Details.question}`, components: [row]})
-    }
+            let Query;
+            const result = await fetch("https://opentdb.com/api.php?amount=1");
+            Query = await result.json();
+        
+            if (!Query.results) {
+                console.log("Couldn't retrieve data");
+                await interaction.reply({ content: "Failed to access trivia database. Try again later" });
+                return;
+            }
+        
+            const Details = Query.results[0];
+            console.log(Details);
+        
+            const row = new ActionRowBuilder();
+            const buttons = [
+                new ButtonBuilder()
+                    .setCustomId("CorrectAnswer")
+                    .setLabel(Details.correct_answer)
+                    .setStyle('Secondary')
+                    .setDisabled(false),
+        
+                ...Details.incorrect_answers.map(answer => new ButtonBuilder()
+                    .setCustomId(answer)
+                    .setLabel(answer)
+                    .setStyle('Secondary')
+                    .setDisabled(false))
+            ];
+        
+            // Randomize the position of the buttons using splice
+            for (let i = buttons.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [buttons[i], buttons[j]] = [buttons[j], buttons[i]];
+            }
+            
+            row.addComponents(buttons);
+        
+            // Defer the reply until later in the code
+            await interaction.deferReply();
+        
+            // Create a collector to handle button clicks
+            const filter = (interaction) => interaction.user.id === interaction.member.id;
+            const collector = interaction.channel.createMessageComponentCollector({
+                filter, componentType: ComponentType.Button, time: 15000 // 15 second timeout
+            });
+        
+            // Handle button clicks
+            collector.on("collect", async (interaction) => {
+                console.log("Clicked button");
+            
+                // Find the button that was clicked
+                const clickedButton = buttons.find(button => button.customId === interaction.customId);
+            
+                // Disable the button that was clicked
+                if (clickedButton) {
+                    clickedButton.setDisabled(true);
+                    const clickedComponent = row.components.find(component => component.customId === clickedButton.customId);
+                    if (clickedComponent) {
+                        clickedComponent.update({ disabled: true });
+                    }
+                }
+            
+                await interaction.update({ components: [row] });
+            
+                if (interaction.customId === "CorrectAnswer") {
+                    await interaction.editReply({ content: "Correct!" });
+                } else {
+                    await interaction.editReply({ content: "Incorrect." });
+                }
+            
+                collector.stop();
+            });
+        
+            // Enable the buttons, then edit the initial reply
+            for (const button of buttons) {
+                button.setDisabled(false);
+            }
+            await interaction.editReply({ content: `${Details.question}`, components: [row], ephemeral: true });
+        }
+    
 }
