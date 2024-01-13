2d16185bf105946075c01eda7f60c9f4f077bc92
theaxelchoco <axelwalejames@gmail.com>
2024-01-12 20:30:55 -0400

    Translate Function
    This command translates the input text into a different language that is chosen amongst the options. It uses the google cloud and google's translate api to acheive this. Currently only translates to french.

--- commands/translate.js
+++ commands/translate.js
@@ -1,5 +1,19 @@
 const {SlashCommandBuilder, Guild, EmbedBuilder} = require("discord.js")
-const {request, fetch} = require("undici")
+const {Translate} = require('@google-cloud/translate').v2
+
+const Translator = new Translate()
+async function TranslateText(Text, Language) {
+    console.log(Text, Language)
+    let [Translations] = await Translator.translate(Text, Language)
+    Translations = Array.isArray(Translations) ? Translations: [Translations]
+
+    let Response = ""
+    Translations.forEach((Translation, i) => {
+        Response += Translation
+    });
+
+    return Response
+}
 
 module.exports = {
     cooldown: 10,
@@ -10,8 +24,24 @@ module.exports = {
 			option.setName('message')
 				.setDescription('The message to be translated.')
 				.setRequired(true)),
+        /*
+        .addStringOption(option =>
+            option.setName('language')
+                .setDescription('The language to be translated to.')
+                .setRequired(true))
+                .addChoices(
+                    {name: "fe"}
+                ),
+        */
     
     async execute(interaction) {
-        
+        const InputText = interaction.options.getString('message' , true)
+        const Language = "fr" // interaction.options.getString('language', true)
+        console.log(InputText)
+
+        TranslateText(InputText, Language)
+            .then(Translation => {
+                interaction.reply({content: Translation})
+            } )
     }
 }
